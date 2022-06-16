9912ebeb3b6bfe3df77f677986e13fe02e1c19ae
Nattapat-Tatiyarat <peak_ntp@hotmail.com>
2022-06-17 00:59:11 +0700

    make it better

--- configs/connect.js
+++ configs/connect.js
@@ -5,8 +5,10 @@ require("dotenv").config();
 // const MONGOOSE_URL = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
 // const MONGOOSE_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@shippop.9nzie.mongodb.net/${process.env.MONGO_DB}`;
 // const MONGOOSE_URL = `mongodb://localhost:27017/Shippop`;
-const MONGOOSE_URL = `mongodb+srv://Peakntp:12341234@shippop.9nzie.mongodb.net/Shippop`;
+// const MONGOOSE_URL = `mongodb+srv://Peakntp:12341234@shippop.9nzie.mongodb.net/Shippop`;
 
+const MONGOOSE_URL = `mongodb+srv://Peakntp:12341234@shippop.9nzie.mongodb.net/?retryWrites=true&w=majority`;
+// mongodb+srv://Peakntp:12341234@shippop.9nzie.mongodb.net/?retryWrites=true&w=majority
 const MONGOOSE_OPTIONS = {
   useNewUrlParser: true,
 };
