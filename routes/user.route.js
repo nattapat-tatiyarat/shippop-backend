6c21a69999cde927d9313341125dec4c8a9961a6
Nattapat-Tatiyarat <peak_ntp@hotmail.com>
2022-06-17 01:46:18 +0700

    final update

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,16 +5,5 @@ const { UserController } = require("../controllers");
 router.post("/get-all", UserController.getAll);
 router.post("/create", UserController.create);
 router.post("/delete", UserController.delete);
-router.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: "HELLO PEAKAAA",
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Server error");
-  }
-});
 
 module.exports = router;
