6fbd5e4e33bd47a685868e3d94799103240005d8
Nattapat-Tatiyarat <peak_ntp@hotmail.com>
2022-06-17 00:38:26 +0700

    test

--- configs/connect.js
+++ configs/connect.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 // mongodb+srv://Peakntp:*****@shippop.9nzie.mongodb.net/test
 // const MONGOOSE_URL = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
 // const MONGOOSE_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@shippop.9nzie.mongodb.net/${process.env.MONGO_DB}`;
-const MONGOOSE_URL = `mongodb://localhost:27017/Shippop`;
+// const MONGOOSE_URL = `mongodb://localhost:27017/Shippop`;
+const MONGOOSE_URL = `mongodb+srv://Peakntp:12341234@shippop.9nzie.mongodb.net/Shippop`;
 
 const MONGOOSE_OPTIONS = {
   useNewUrlParser: true,
